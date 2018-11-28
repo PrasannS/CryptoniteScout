cdd6e1eac5f59c8c388ddf92c8fd89bc0a2f7548
TaeminHa <tommy7507@gmail.com>
2018-11-27 20:59:19 -0600

    Hi

--- app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/Models/PregameEntry.java
+++ app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/Models/PregameEntry.java
@@ -4,9 +4,16 @@ public class PregameEntry extends DataEntry {
     public PregameEntry(){
         super();
     }
+    public int matchNumber;
+    public boolean noShow = false;
+    public boolean LostCommns = false;
 
     @Override
     public void setTeamnum(String tn) {
         super.setTeamnum(tn);
+    }
+    public PregameEntry (){};
+    public PregameEntry (int teamNum){
+        teamNum = ;
     }
 }
