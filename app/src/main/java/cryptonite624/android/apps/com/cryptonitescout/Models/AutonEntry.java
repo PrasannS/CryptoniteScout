c6348f2e9f250712bf530e70db4a18b122d39b8d
TaeminHa <tommy7507@gmail.com>
2018-11-27 20:59:36 -0600

    Hi

--- app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/Models/AutonEntry.java
+++ app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/Models/AutonEntry.java
@@ -1,7 +1,27 @@
 package cryptonite624.android.apps.com.cryptonitescout.Models;
 
+import android.widget.Button;
+
 public class AutonEntry extends DataEntry {
+
+    public boolean baslineCrossed = false;
+    public boolean AutoSwitchAttempt = false;
+    public boolean AutonSscaleAttemp = false;
+    public String AutonNotes = "";
+    public int AutonSwitchCubes;
+    public int AutonScaleCubes;
+    public int StartingPosition;
+    // 0 = not selected, 1 = Left, 2 = Center, 3 = Right;
+
     public AutonEntry(){
         super();
     }
+     public void setTeamNum(String tn){
+        super.setTeamnum(tn);
+     }
+
+     AutonEntry autonEntry = new AutonEntry();
+
+
 }
+
