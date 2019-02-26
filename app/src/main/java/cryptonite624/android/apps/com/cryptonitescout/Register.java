be1aae052dd6a1df308572b07c7ae921ac8da8eb
TaeminHa <tommy7507@gmail.com>
2019-02-25 19:58:45 -0600

    toString and parseUser methods for User datas are done!!

--- app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/Register.java
+++ app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/Register.java
@@ -26,8 +26,4 @@ public class Register extends AppCompatActivity {
         });
     }
 
-    public String userDatatoString(User data){
-        String datas = "hi";
-        return datas;
-    }
 }
