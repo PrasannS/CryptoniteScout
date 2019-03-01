af822575dc1303ffef801b427a21e7858fd2cb4b
PrasannS <singhal.prasann07@gmail.com>
2019-03-01 01:27:22 -0600

    Weird replay issues should be fixed

--- app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/SubmissionReviewFragment.java
+++ app/src/main/java/cryptonite624/android/apps/com/cryptonitescout/SubmissionReviewFragment.java
@@ -94,7 +94,7 @@ public class SubmissionReviewFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_submission_review, container, false);
 
-        replayButton = view.findViewById(R.id.openreplay);
+        //replayButton = view.findViewById(R.id.openreplay);
         totalHatches = view.findViewById(R.id.submission_hatchnum);
         totalCargos = view.findViewById(R.id.submission_cargonum);
         climbLevel = view.findViewById(R.id.submission_hablevel);
@@ -117,12 +117,12 @@ public class SubmissionReviewFragment extends Fragment {
             }
         });
 
-        replayButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                submissionReadListener.OnSubmissionRead("replay");
-            }
-        });
+//        replayButton.setOnClickListener(new View.OnClickListener() {
+  //          @Override
+  //          public void onClick(View v) {
+  //              submissionReadListener.OnSubmissionRead("replay");
+  //          }
+  //      });
 
 
 
